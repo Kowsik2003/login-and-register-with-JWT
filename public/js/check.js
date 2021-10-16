fe2d37ace7a0817069792a032f17d355eb57b6f0
Kowsik S <68153590+Kowsik2003@users.noreply.github.com>
2021-10-16 16:47:05 +0530

    Update check.js

--- public/js/check.js
+++ public/js/check.js
@@ -38,6 +38,6 @@ document.getElementById('btn').addEventListener('click',async  e => {
 	const token = localStorage.getItem('jwt');
 	if(!token)
 		return alert('User does not login');
-	await postData('http://127.0.0.1:1000/check',{token});
+	await postData('https://kowsik-2003.herokuapp.com/check',{token});
 	//console.log(token);
 });
