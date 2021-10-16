3edfed7bfdd14024797ded107212e8d40ad6b7e3
Kowsik S <68153590+Kowsik2003@users.noreply.github.com>
2021-10-16 15:35:29 +0530

    Add files via upload

--- app.js
+++ app.js
@@ -1,42 +1,116 @@
-const express = require('express');
 const path = require('path');
-const dotenv = require('dotenv');
-const pug = require('pug');
-const mongoose = require('mongoose');
-const bodyParser = require('body-parser');
-const cors = require('cors');
+const express = require('express');
 const morgan = require('morgan');
+const rateLimit = require('express-rate-limit');
+const helmet = require('helmet');
+const mongoSanitize = require('express-mongo-sanitize');
+const xss = require('xss-clean');
+const hpp = require('hpp');
+const cookieParser = require('cookie-parser');
+const bodyParser = require('body-parser');
+const compression = require('compression');
+const cors = require('cors');
 
-const webCtrl = require(`${__dirname}/controller/webCtrl`); 
-const errCtrl = require(`${__dirname}/controller/errorCtrl`);
+const AppError = require('./utils/appError');
+const globalErrorHandler = require('./controllers/errorController');
+const tourRouter = require('./routes/tourRoutes');
+const userRouter = require('./routes/userRoutes');
+const reviewRouter = require('./routes/reviewRoutes');
+const bookingRouter = require('./routes/bookingRoutes');
+const bookingController = require('./controllers/bookingController');
+const viewRouter = require('./routes/viewRoutes');
 
+// Start express app
 const app = express();
-dotenv.config({path : './config.env'})
 
-app.use(bodyParser.urlencoded({ extended: true })); 
+app.enable('trust proxy');
 
-app.use(express.json());
+app.set('view engine', 'pug');
+app.set('views', path.join(__dirname, 'views'));
+
+// 1) GLOBAL MIDDLEWARES
+// Implement CORS
 app.use(cors());
-app.use(morgan('dev'));
+// Access-Control-Allow-Origin *
+// api.natours.com, front-end natours.com
+// app.use(cors({
+//   origin: 'https://www.natours.com'
+// }))
 
-app.set('view engine','pug');
-app.set('views',path.join(__dirname,'views'));
-app.use(express.static(`${__dirname}/public`));
+app.options('*', cors());
+// app.options('/api/v1/tours/:id', cors());
 
+// Serving static files
+app.use(express.static(path.join(__dirname, 'public')));
 
-//Routes
+// Set security HTTP headers
+app.use(helmet());
 
-app.route('/').get(webCtrl.loginPage).post(webCtrl.authUser);
+// Development logging
+if (process.env.NODE_ENV === 'development') {
+  app.use(morgan('dev'));
+}
 
-app.route('/check').get(webCtrl.checkLogin).post(webCtrl.verifyToken);
+// Limit requests from same API
+const limiter = rateLimit({
+  max: 100,
+  windowMs: 60 * 60 * 1000,
+  message: 'Too many requests from this IP, please try again in an hour!'
+});
+app.use('/api', limiter);
 
-app.route('/register').get(webCtrl.regPage).post(webCtrl.regUser);
+// Stripe webhook, BEFORE body-parser, because stripe needs the body as stream
+app.post(
+  '/webhook-checkout',
+  bodyParser.raw({ type: 'application/json' }),
+  bookingController.webhookCheckout
+);
 
-app.use(errCtrl);
-dotenv.config({path : path.join(__dirname,'config.env')})
+// Body parser, reading data from body into req.body
+app.use(express.json({ limit: '10kb' }));
+app.use(express.urlencoded({ extended: true, limit: '10kb' }));
+app.use(cookieParser());
 
-mongoose.connect(process.env.MONGODB_CLOUD_DB).then(() => console.log('DB connected..')).catch(err => console.log(err));
+// Data sanitization against NoSQL query injection
+app.use(mongoSanitize());
 
-app.listen(1000,() => {
-	console.log('TODO running ...');
-}); 
+// Data sanitization against XSS
+app.use(xss());
+
+// Prevent parameter pollution
+app.use(
+  hpp({
+    whitelist: [
+      'duration',
+      'ratingsQuantity',
+      'ratingsAverage',
+      'maxGroupSize',
+      'difficulty',
+      'price'
+    ]
+  })
+);
+
+app.use(compression());
+
+// Test middleware
+app.use((req, res, next) => {
+  req.requestTime = new Date().toISOString();
+  // console.log(req.cookies);
+  next();
+});
+
+// 3) ROUTES
+app.use('/', viewRouter);
+app.use('/api/v1/tours', tourRouter);
+app.use('/api/v1/users', userRouter);
+app.use('/api/v1/reviews', reviewRouter);
+app.use('/api/v1/bookings', bookingRouter);
+
+app.all('*', (req, res, next) => {
+  next(new AppError(`Can't find ${req.originalUrl} on this server!`, 404));
+});
+
+app.use(globalErrorHandler);
+
+module.exports = app;
